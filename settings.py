b207251b7ad0a6f78b8aa5e3bcb697304f960f42
Михайлов Артём <artem.mikh100@gmail.com>
2024-11-07 10:55:48 +0300

    Исправление ENDPOINT

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '1234test4321'
     PATTERN = r'^[A-Za-z0-9]{1,16}$'
-    ENDPOINT = 'http://localhost/'
+    ENDPOINT = 'http://127.0.0.1:5000/'
